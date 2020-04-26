9bd9a33cc085eeebe2a3dd2f61e1608f295d19eb
walker <warriorsfly@gmail.com>
2020-04-26 15:22:47 +0800

    init project

---
+++ src/main/java/com/mimosa/firedance/DancingApplication.java
@@ -0,0 +1,13 @@
+package com.mimosa.firedance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DancingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DancingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mimosa/firedance/DancingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mimosa.firedance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DancingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
